c34b26603a160df1b713a63f5e87f3d86991c3c9
ManuelRojasDev <e.j.kulker@student.tue.nl>
2023-12-07 13:00:17 +0100

    server and client
    camera.py == camera handler
    server.py ==  server handler to run on raspberry pi
    client.py to run on remote computer

---
+++ client.py
@@ -0,0 +1,72 @@
+import argparse
+import json
+import pickle
+import time
+import cv2
+import numpy
+from websockets.sync.client import connect
+import keyboard
+import socket
+
+
+class Client:
+    def __init__(self, host: str = "ws://localhost:8000/"):
+        self.host = host
+
+    def pass_get_states(self) -> dict:
+        with connect(self.host) as websocket:
+            request = {"motor_control": self.read_keyboard_states()}
+            request = pickle.dumps(request)
+
+            websocket.send(request)
+            message = websocket.recv()
+            data = pickle.loads(message)
+            response = {
+                "status": True,
+                "cam": data["cam"],
+                "sensors": data["sensors"]
+            }
+            return response
+
+    def read_keyboard_states(self) -> dict:
+        keypressed = {
+            'forward': keyboard.is_pressed("w"),
+            'backwards': keyboard.is_pressed("s"),
+            'right': keyboard.is_pressed("a"),
+            'left': keyboard.is_pressed("d"),
+        }
+        return keypressed
+
+    def show_image(self, frame: numpy.array):
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
+        return frame
+
+    def run(self):
+        print(f"running client with target host: {self.host}")
+        while 1:
+            data = self.pass_get_states()
+            self.show_image(frame=data["cam"])
+            print(data["sensors"])
+            time.sleep(1 / 30)
+
+parser = argparse.ArgumentParser(
+    prog='Client',
+    description='remote controller over websocket',
+    epilog='yoooooo')
+parser.add_argument('-ip')
+parser.add_argument('-port')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    IP = args.ip
+    PORT = args.port
+
+    if IP == None:
+        host = socket.gethostname()
+        IP = socket.gethostbyname(host)
+        PORT = 8000
+    PORT = 8000 if PORT == None else PORT
+    HOST = f"ws://{IP}:{PORT}/"
+    client = Client(HOST)
+    client.run()
---
+++ run.sh
@@ -0,0 +1,4 @@
+sudo pip install websockets
+sudo pip install RPi.GPIO
+sudo pip install opencv-python
+sudo python server.py
